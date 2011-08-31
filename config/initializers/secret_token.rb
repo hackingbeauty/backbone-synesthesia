5c91231416b32f25aeca4945528f19692424fde5
eaturspinach <markmuskardin@gmail.com>
2011-08-31 15:10:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneSynesthesia::Application.config.secret_token = '19caac1b404b0191c9c551cca8e4b611ecada0d92095a532f9c3a8c15c24a2bc8398704d230f0c067c4fc5be81618c220acce4a0925d7348c65803c5d3185bb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneSynesthesia::Application.config.session_store :cookie_store, key: '_backbone-synesthesia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BackboneSynesthesia::Application.config.session_store :active_record_store
